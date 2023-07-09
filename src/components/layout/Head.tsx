9f67166333fae4d78e9312a173ade9471bbfa0df
julienbrg <julien@strat.cc>
2023-07-09 16:01:37 +0200

    fix seo

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -10,7 +10,15 @@ interface Props {
 export function Head(props: Props) {
   return (
     <NextHead>
-      <title>{props.title ?? SITE_NAME}</title>
+      <title>
+        {props.title ?? SITE_NAME} {'v0.1.0-alpha'}{' '}
+      </title>
+      <meta property="og:url" content="https://arthera-lucky-wheel.on.fleek.co/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={props.title ?? SITE_NAME} />
+      <meta name="twitter:card" content={SITE_DESCRIPTION} />
+      <meta property="og:description" content={props.description ?? SITE_DESCRIPTION} />
+      <meta property="og:image" content={'https://bafybeiaswv3numcgmtcby6mgdlurmzisi5fzzujozyistx6ozqvutrdony.ipfs.w3s.link/arthera-logo.png'} />
       <meta name="description" content={props.description ?? SITE_DESCRIPTION} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </NextHead>
