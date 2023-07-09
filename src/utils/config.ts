1d754aae41eec563b2981acb930973c32a69eb9d
julienbrg <julien@strat.cc>
2023-07-09 14:20:16 +0200

    initial setup

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,8 @@
 import { ThemingProps } from '@chakra-ui/react'
 import { goerli } from '@wagmi/chains'
 
-export const SITE_NAME = 'Another Star'
-export const SITE_DESCRIPTION = 'A Web3 app boilerplate built with Next.js, Chakra UI, Ethers, Wagmi and ConnectKit'
+export const SITE_NAME = 'Arthera Lucky Wheel'
+export const SITE_DESCRIPTION = 'Spin the wheel and get some AA'
 export const SITE_URL = 'https://w3hc.org'
 export const THEME_INITIAL_COLOR = 'dark'
 export const THEME_COLOR_SCHEME: ThemingProps['colorScheme'] = 'gray'
@@ -11,7 +11,7 @@ export const THEME_CONFIG = {
 }
 
 export const SOCIAL_TWITTER = 'W3HC'
-export const SOCIAL_GITHUB = 'w3hc/nexth'
+export const SOCIAL_GITHUB = 'w3hc/arthera-lucky-wheel'
 
 export const ETH_CHAINS = [goerli]
 export const alchemyId = process.env.NEXT_PUBLIC_ARBITRUM_ALCHEMY_ID
